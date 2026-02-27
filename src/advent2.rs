993dd1ae91d6615a817d4c29f07788e6c353bf2f
Tobias Klingenberg <tobikli@pm.me>
2026-02-27 23:57:44 +0100

    Add Day 2 Part 2

--- src/advent2.rs
+++ src/advent2.rs
@@ -5,7 +5,8 @@ pub fn main(lines: Vec<String>) {
     println!("Advent of Code Day 2");
     let single_line = lines.get(0).unwrap();
     let ranges = single_line.split(",");
-    let mut sum = 0;
+    let mut sum1 = 0;
+    let mut sum2 = 0;
     for range in ranges {
         let mut split = range.split("-");
         let start_string = split.next().unwrap();
@@ -13,15 +14,19 @@ pub fn main(lines: Vec<String>) {
         let start: u64 = start_string.parse::<u64>().unwrap();
         let end: u64 = end_string.parse::<u64>().unwrap();
         for n in start..=end {
-            if number_is_invalid(n) {
-                sum += n;
+            if number_is_invalid_part1(n) {
+                sum1 += n;
+            }
+            if number_is_invalid_part2(n) {
+                sum2 += n;
             }
         }
     }
-    println!("Result: {}", sum);
+    println!("Result Part1: {}", sum1);
+    println!("Result Part2: {}", sum2);
 }
 
-fn number_is_invalid(input: u64) -> bool {
+fn number_is_invalid_part1(input: u64) -> bool {
     let input_string = input.to_string();
     let length = input_string.chars().count();
     if length % 2 != 0 {
@@ -29,4 +34,27 @@ fn number_is_invalid(input: u64) -> bool {
     }
     let split = input_string.split_at(length / 2);
     return split.0 == split.1;
+}
+
+fn number_is_invalid_part2(input: u64) -> bool {
+    let input_string = input.to_string();
+    let length = input_string.chars().count();
+    for i in 1..=length / 2 {
+        if length % i != 0 {
+            continue;
+        }
+        let mut temp: Vec<String> = Vec::new();
+        let mut string_copy = input_string.clone();
+        while !string_copy.is_empty() {
+            let rest = string_copy.split_off(i);
+            temp.push(string_copy);
+            string_copy = rest;
+        }
+
+        let pattern = temp.get(0).unwrap();
+        if temp.iter().map(|x| x == pattern).all(|x| x) {
+            return true;
+        }
+    }
+    return false;
 }
